60e2b4e060462dbbc561d7000e2555fe7dcbf512
Manuel Garrido <mgc@apeiroo.com>
2025-01-02 17:02:11 +0100

    starting the app, enjoy :)

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Calendar } from 'react-native-calendars'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Calendar
+        onDayPress={(day) => {
+          console.log('Selected day', day)
+        }}
+        markedDates={
+          {
+            '2025-01-01': {selected: true, marked: true, selectedColor: 'blue'},
+          }
+        }
+      />
     </View>
   );
 }
