cf71b7d81188ad9c59f77b266c4271343929796d
Manugarricas <manugarricas@gmail.com>
2025-01-02 17:26:15 +0100

    minor changes, just a test

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { Calendar } from 'react-native-calendars'
 
 export default function App() {
+
+  const [events, setEvents] = useState(
+    {
+    '2025-01-02': { marked: true, dotColor: 'red' },
+})
+
+  const addEvent = () => {
+    setEvents({
+      ...events,
+      '2025-01-03': { marked: true, dotColor: 'green' },
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -12,11 +26,10 @@ export default function App() {
           console.log('Selected day', day)
         }}
         markedDates={
-          {
-            '2025-01-01': {selected: true, marked: true, selectedColor: 'blue'},
-          }
+          events
         }
       />
+      <Button title='Agregar evento' onPress={addEvent} />
     </View>
   );
 }
